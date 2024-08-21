dcfcee17d8058245eec5312904132db973484841
Vinycius Marques <vinyciuscezar@outlook.com>
2024-08-21 17:31:00 -0300

    cadastro

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -5,14 +5,17 @@ using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace SimuladorSaque
 {
     public class Cliente
     {
+        private static List<Cliente> clientes = new List<Cliente>();
+
         public string Nome { get; set; }
         public string CPF { get; set; }
-        public string Senha { get; set; }
+        internal string Senha { get; set; }
         public double Saldo { get; set; }
 
         public virtual void Add()
@@ -20,28 +23,34 @@ namespace SimuladorSaque
             Nome = Console.ReadLine();
             CPF = (Console.ReadLine());
             Senha = Console.ReadLine();
-            Saldo = int.Parse(Console.ReadLine());
+            Saldo = double.Parse(Console.ReadLine());
         }
 
-        private void SetSenha(string senha)
+        void CadastraCliente(string cliente)
         {
 
-            string Senha = Console.ReadLine();
+            List<Cliente> clientes = new List<Cliente>();
+            {
+                Console.WriteLine("Digite o seu nome:");
+                Nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a sua senha:");
+                Senha = Console.ReadLine();
+                Senha = HashSenha(Senha);
+
+                clientes.Add(this);
+            }
+        }
+        private static string HashSenha(string Senha)
+        {
+
             using (var sha256 = SHA256.Create())
             {
                 byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(Senha));
-                Senha = Convert.ToBase64String(hashedBytes);
+                return Convert.ToBase64String(hashedBytes);
             }
-
         }
-
 
     }
 
-
-
 }
-
-
-
-
